ec44bebc08e3af8a1f48efbe2ca67782faae5450
jcode89 <jv.colon@yahoo.com>
2015-09-20 17:20:30 -0400

    Removes unnecessary loops and if/else statements.

--- mad_libs.py
+++ mad_libs.py
@@ -22,33 +22,21 @@ def create_story(choice, words):
                 ${pronoun}, ${verb} is absolutely amazing!""",
                 """$noun1 will be doing $adj1 things. $verb is $adj2 for $noun2."""]
     if choice == "story1":
-        for text in words:
-            if text == words[0]:
-                # The code '\033[31m' is the start sequence
-                # and color code for the text.
-                # '\033[0m' is the end sequence.
-                adj = '\033[31m' + text + '\033[0m'
-            elif text == words[1]:
-                noun = '\033[31m' + text + '\033[0m'
-            elif text == words[2]:
-                pronoun = '\033[31m' + text + '\033[0m'
-            elif text == words[3]:
-                verb = '\033[31m' + text + '\033[0m'
-        temp = story_temps[0]
-        templ = Template(temp)
+        # The code '\033[31m' is the start sequence
+        # and color code for the text.
+        # '\033[0m' is the end sequence.
+        adj = '\033[31m' + words[0] + '\033[0m'
+        noun = '\033[31m' + words[1] + '\033[0m'
+        pronoun = '\033[31m' + words[2] + '\033[0m'
+        verb = '\033[31m' + words[3] + '\033[0m'
+        templ = Template(story_temps[0])
         print(templ.safe_substitute(adj=adj, noun=noun, pronoun=pronoun, verb=verb))
     elif choice == "story2":
-        for text in words:
-            if text == words[0]:
-                n = '\033[31m' + text + '\033[0m'
-            elif text == words[1]:
-                a = '\033[31m' + text + '\033[0m'
-            elif text == words[2]:
-                v = '\033[31m' + text + '\033[0m'
-            elif text == words[3]:
-                a2 = '\033[31m' + text + '\033[0m'
-            elif text == words[4]:
-                n2 = '\033[31m' + text + '\033[0m'
+        n = '\033[31m' + words[0] + '\033[0m'
+        a = '\033[31m' + words[1] + '\033[0m'
+        v = '\033[31m' + words[2] + '\033[0m'
+        a2 = '\033[31m' + words[3] + '\033[0m'
+        n2 = '\033[31m' + words[4] + '\033[0m'
         temp = story_temps[1]
         templ = Template(temp)
         print(templ.safe_substitute(noun1=n, adj1=a, verb=v, adj2=a2, noun2=n2))
